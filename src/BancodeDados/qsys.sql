828493bb14ffa4ad2713bf84ef53346a7b902d83
XPerience <XPerience@20.20.0.212>
2017-05-31 11:35:37 -0300

    Desenvolvimento do projeto. - Atualizações.

---
+++ src/BancodeDados/qsys.sql
@@ -0,0 +1,10 @@
+create database qsys
+default character set utf8
+default collate utf8_general_ci;
+
+
+
+create table administrador(
+nome VARCHAR (50) not null,
+id INT primary key not null
+)default charset = utf8;
